f9c5f8eb9959b87373da949b236fbb4a904512be
vezei <sadbhavadhikari@gmail.com>
2023-03-14 16:56:01 +0545

    [refactor] make site repo link descriptive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,18 @@
 import "../styles/Footer.css";
 
 function Footer() {
-    return (
-        <div>
-            <hr />
-            <p className="footerText">
-                repo for this webpage is <a href="https://www.github.com/pes18fan/md-prev" target="_blank">here</a>.
-            </p>
-        </div>
-    )
+  return (
+    <div>
+      <hr />
+      <p className="footerText">
+        visit this site's{" "}
+        <a href="https://www.github.com/pes18fan/md-prev" target="_blank">
+          repository
+        </a>
+        .
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
